af191ea7d2e6ec2b20907c1f9bd15555b5328b95
Oliver Caldwell <olliec87@gmail.com>
2015-04-13 20:50:15 +0100

    Added initial CSV module from the book

---
+++ ruby/day3/csv.rb
@@ -0,0 +1,31 @@
+# The base class is from the book.
+# I'm adding the each method and CsvRow class.
+module ActsAsCsv
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def acts_as_csv
+      include InstanceMethods
+    end
+  end
+
+  module InstanceMethods
+    def read
+      @csv_contents = []
+      filename = "#{self.class.to_s.downcase}.txt"
+      file = File.new(filename)
+      @headers = file.gets.chomp.split(', ')
+
+      file.each do |row|
+        @csv_contents << row.chomp.split(', ')
+      end
+    end
+
+    attr_accessor :headers, :csv_contents
+    def initialize
+      read
+    end
+  end
+end
