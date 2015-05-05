06132ab040dca3463de059f9cf8175f4395eb031
Oliver Caldwell <olliec87@gmail.com>
2015-05-05 22:31:39 +0100

    Removed core. Using day named files.

--- clojure/src/clj_lang/core.clj
+++
@@ -1,6 +0,0 @@
-(ns clj-lang.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- clojure/test/clj_lang/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clj-lang.core-test
-  (:require [clojure.test :refer :all]
-            [clj-lang.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
