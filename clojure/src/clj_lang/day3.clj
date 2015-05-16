27b83b13d30f2116c879561012f526328a5872b5
Oliver Caldwell <olliec87@gmail.com>
2015-05-16 20:03:33 +0100

    Began Clojure barber problem

--- clojure/src/clj_lang/day3.clj
+++ clojure/src/clj_lang/day3.clj
@@ -13,3 +13,36 @@
 
 (defn debit [n amount]
   (alter-acct n amount -))
+
+;; <problem>
+;; A barber shop takes customers
+;; Customers arrive at random intervals, from 10 to 30 milliseconds.
+;; The barber shop has three chairs in the waiting room.
+;; The barber shop has one barber and one barber chair.
+;; When the barber's chair is empty a customer sits in the chair, wakes up the barber and gets a haircut.
+;; If the chairs are occupied, all new customers will turn away.
+;; Haircuts take 20 milliseconds.
+;; After a customer receives a haircut he gets up and leaves.
+
+;; Write a multithreaded program to determine how many haircuts a barber can give in 10 seconds.
+;; </problem>
+
+;; I wish my haircuts were that fast.
+
+(atom barbers-chair nil)
+(atom waiting-chairs nil)
+(atom hair-cuts nil)
+
+(defn rand-int-between [low high]
+  (+ (rand-int (- (inc low) high)) high))
+
+(defn next-customer-timeout []
+  (rand-int-between 10 30))
+
+(def haircut-timeout 20)
+(def test-timeout (* 10 1000))
+
+(defn baber-problem []
+  (reset! barbers-chair nil)
+  (reset! waiting-chairs [])
+  (reset! hair-cuts 0))
