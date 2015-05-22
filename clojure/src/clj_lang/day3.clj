e70f650c2b83ccbb774a967035eef97cdfd448fe
Oliver Caldwell <olliec87@gmail.com>
2015-05-22 17:07:49 +0100

    Added example barber problem solution

--- clojure/src/clj_lang/day3.clj
+++ clojure/src/clj_lang/day3.clj
@@ -29,20 +29,46 @@
 
 ;; I wish my haircuts were that fast.
 
-(atom barbers-chair nil)
-(atom waiting-chairs nil)
-(atom hair-cuts nil)
+;; Stolen from: https://gist.github.com/kachayev/3160721
+;; Just wanted a solution to learn from.
 
-(defn rand-int-between [low high]
-  (+ (rand-int (- (inc low) high)) high))
+(def open-for-business? (atom true))
+(def haircut-count (agent 0))
+(def waiting-room (ref []))
+(def waiting-room-size 3)
 
-(defn next-customer-timeout []
-  (rand-int-between 10 30))
+(defn open-shop [duration]
+  (do (Thread/sleep duration)
+      (swap! open-for-business? not)))
 
-(def haircut-timeout 20)
-(def test-timeout (* 10 1000))
+(defn add-customers []
+  (future
+    (while @open-for-business?
+      (println "Waiting Room:" @waiting-room)
+      (dosync
+        (if
+          (< (count (ensure waiting-room)) waiting-room-size)
+          (alter waiting-room conj :customer)))
+      (Thread/sleep (+ 10 (rand-int 21))))))
 
-(defn baber-problem []
-  (reset! barbers-chair nil)
-  (reset! waiting-chairs [])
-  (reset! hair-cuts 0))
+(defn get-next-customer []
+  (dosync
+    (let [next-customer (first (ensure waiting-room))]
+      (when next-customer (alter waiting-room rest) next-customer))))
+
+(defn cut-hair []
+  (future
+    (while @open-for-business?
+      (when-let [next-customer (get-next-customer)]
+        (Thread/sleep 20)
+        ;; do we want to continue cut hair before (!)
+        ;; the actual value is set (possibly) ???
+        (send haircut-count inc)))))
+
+(defn barber-problem []
+  (cut-hair)
+  (add-customers)
+  (println "Open barber shop for 10 secs")
+  (open-shop 10000)
+  (println "Number of cuts:" @haircut-count)
+  (System/exit 0))
